cf5c1c0e6ab8ded02b5a7099409acbdde7ee81b6
banderastoni <banderastoni@rambler.ru>
2023-06-15 01:03:35 +0300

    100% test coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,8 +4,11 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == 'Array is empty'
-
+    assert arrs.get([1, 2, 3], -1, "out of range") == "out of range"
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 0) == []
+    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
